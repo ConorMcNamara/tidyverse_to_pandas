15ccbcf21445caed610957d8fa70df1200ebaadb
Conor McNamara <conor.s.mcnamara@gmail.com>
2023-07-27 12:18:20 -0700

    Added type hints

--- test/test_helper_select.py
+++ test/test_helper_select.py
@@ -7,18 +7,18 @@ import pytest
 
 class TestHelperSelect(unittest.TestCase):
 
-    def test_everything(self):
+    def test_everything(self) -> None:
         cars = pd.read_csv('C:\\Users\\conor\\Documents\\tidyverse_to_pandas\\data\\mtcars.csv')
         cars = cars.rename({'Unnamed: 0': 'car'}, axis=1)
         expected = ['car', 'mpg', 'cyl', 'disp', 'hp', 'drat', 'wt', 'qsec', 'vs', 'am', 'gear', 'carb']
         np.testing.assert_array_equal(everything(cars), expected)
 
-    def test_lastCol_noOffset(self):
+    def test_lastCol_noOffset(self) -> None:
         cars = pd.read_csv('C:\\Users\\conor\\Documents\\tidyverse_to_pandas\\data\\mtcars.csv')
         cars = cars.rename({'Unnamed: 0': 'car'}, axis=1)
         assert last_col(cars) == 'carb'
 
-    def test_lastCol_offset(self):
+    def test_lastCol_offset(self) -> None:
         cars = pd.read_csv('C:\\Users\\conor\\Documents\\tidyverse_to_pandas\\data\\mtcars.csv')
         cars = cars.rename({'Unnamed: 0': 'car'}, axis=1)
         assert last_col(cars, 2) == 'am'
